f2343cdebc281ae993c0be75314a8618f0191ed9
sdc9787 <106896972+sdc9787@users.noreply.github.com>
2023-10-31 11:36:19 +0900

    add analytics

--- src/App.js
+++ src/App.js
@@ -66,63 +66,7 @@ function App() {
     checked.forEach((b) => {
       //체크한 직업의 스킬만큼 반복
       classSkill[b].forEach((a) => {
-        var XMLHttpRequest = require("xhr2");
-        var xhr2 = new XMLHttpRequest();
-        xhr2.open("POST", "https://developer-lostark.game.onstove.com/auctions/items", true);
-        xhr2.setRequestHeader("accept", "application/json");
-        xhr2.setRequestHeader("authorization", `bearer ${apiKey.replaceAll(" ", "")}`);
-        xhr2.setRequestHeader("content-Type", "application/json");
-        xhr2.send(
-          JSON.stringify({
-            ItemLevelMin: 0,
-            ItemLevelMax: 0,
-            ItemGradeQuality: null,
-            SkillOptions: [
-              {
-                FirstOption: a.Value,
-                SecondOption: null,
-                MinValue: null,
-                MaxValue: null,
-              },
-            ],
-            EtcOptions: [
-              {
-                FirstOption: null,
-                SecondOption: null,
-                MinValue: null,
-                MaxValue: null,
-              },
-            ],
-            Sort: "BUY_PRICE",
-            CategoryCode: 210000,
-            CharacterClass: b,
-            ItemTier: 3,
-            ItemGrade: null,
-            ItemName: `${gemLevel} ${gemDamCol}`,
-            PageNo: 0,
-            SortCondition: "ASC",
-          })
-        );
-
-        xhr2.onload = () => {
-          let classGem = JSON.parse(xhr2.response); //직업스킬 불러옴
-          count++;
-          if (xhr2.status == 200 && classGem.TotalCount !== 0) {
-            let apiSearchValue = {}; //오브젝트 생성
-            apiSearchValue.skillValue = a.Value; //스킬값 저장
-            apiSearchValue.price = classGem.Items[0].AuctionInfo.BuyPrice; //즉시구매가 저장
-            apiSearchValue.skillName = a.Text; //스킬이름 저장
-            apiSearchValue.className = b; //직업이름 저장
-            apiSearchValue.Icon = a.Icon; //아이콘 경로 저장
-            setGemListAll((gemListAll) => [...gemListAll, apiSearchValue]); //list에 저장
-            setNowClassSkillCount(count);
-            console.log(apiSearchValue);
-          } else if (xhr2.status == 429) {
-            count--;
-            apiResand(a, b);
-          }
-        };
-
+        apiResand(a, b);
         //체크한 직업의 스킬만큼 반복
       });
     });
